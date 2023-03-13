fcd8054a4bc07bd755b466e4fc5ee0ffed2c8bd0
Anastasiia Dotsenko <anastasiia_dots@yahoo.com>
2023-03-14 12:46:18 +1300

    set timer

--- src/modules/one.js
+++ src/modules/one.js
@@ -1,6 +1,21 @@
-const oneName = () => {
-    console.log('my name is One!');
+const one = () => {
+    const timerHours = document.getElementById('timer-hours')
+    const timerMinutes = document.getElementById('timer-minutes')
+    const timerSeconds = document.getElementById('timer-seconds')
+
+    let deadline = '17 march 2023'
+    let dateStop = new Date(deadline).getTime()
+    let dateNow = new Date().getTime()
+    let timeRemaining = (dateStop - dateNow) / 1000
+    let hours = Math.floor(timeRemaining / 60 / 60)
+    let minutes = Math.floor((timeRemaining / 60) % 60)
+    let seconds = Math.floor(timeRemaining % 60)
+
+    timerHours.textContent = hours;
+    timerMinutes.textContent = minutes;
+    timerSeconds.textContent = seconds;
+
 }
 
 
-export default oneName
+export default one
