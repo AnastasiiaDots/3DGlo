60c1d49658e32a11c3c8a8cc78539294a7a12819
Anastasiia Dotsenko <anastasiia_dots@yahoo.com>
2023-03-16 22:19:43 +1300

    added initialization and stop timer

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,45 +1,48 @@
 const timer = (deadline) => {
-    console.log(deadline);
-    const timerHours = document.getElementById('timer-hours')
-    const timerMinutes = document.getElementById('timer-minutes')
-    const timerSeconds = document.getElementById('timer-seconds')
-    // const timerContainer = document.getElementById('timer')
+  console.log(deadline);
+  const timerHours = document.getElementById("timer-hours");
+  const timerMinutes = document.getElementById("timer-minutes");
+  const timerSeconds = document.getElementById("timer-seconds");
 
-    // const timerDays = document.createElement('span');
-    // timerDays.id = 'timer-days';
-    // timerContainer.insertBefore(timerDays, timerHours);
+  const getTimeRemaining = () => {
+    let dateStop = new Date(deadline).getTime();
+    let dateNow = new Date().getTime();
+    let timeRemaining = (dateStop - dateNow) / 1000;
 
-    const getTimeRemaining = () => {
-        let dateStop = new Date(deadline).getTime()
-        let dateNow = new Date().getTime()
-        let timeRemaining = (dateStop - dateNow) / 1000
-
-        if (timeRemaining < 0) {
-            timeRemaining = 0;
-        }
-        // let days = Math.floor(timeRemaining / 60 / 60 / 24)
-        let hours = Math.floor(timeRemaining / 60 / 60)
-        let minutes = Math.floor((timeRemaining / 60) % 60)
-        let seconds = Math.floor(timeRemaining % 60)
-
-        // days = String(hours).padStart(2, '0');
-        hours = String(hours).padStart(2, '0');
-        minutes = String(minutes).padStart(2, '0');
-        seconds = String(seconds).padStart(2, '0');
-
-        return { timeRemaining, hours, minutes, seconds }
+    if (timeRemaining < 0) {
+      timeRemaining = 0;
     }
 
-    const updateClock = () => {
-        setInterval(() => {
-            let getTime = getTimeRemaining();
-            // timerDays.textContent = getTime.days + ' д : ';
-            timerHours.textContent = getTime.hours;
-            timerMinutes.textContent = getTime.minutes;
-            timerSeconds.textContent = getTime.seconds;
-        }, 1000);
-    }
-    updateClock()
-}
+    let hours = Math.floor(timeRemaining / 60 / 60);
+    let minutes = Math.floor((timeRemaining / 60) % 60);
+    let seconds = Math.floor(timeRemaining % 60);
 
-export default timer
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+
+    return { timeRemaining, hours, minutes, seconds };
+  };
+
+  const updateClock = () => {
+    const getTime = getTimeRemaining();
+
+    timerHours.textContent = getTime.hours;
+    timerMinutes.textContent = getTime.minutes;
+    timerSeconds.textContent = getTime.seconds;
+
+    const intervalId = setInterval(() => {
+      let getTime = getTimeRemaining();
+      timerHours.textContent = getTime.hours;
+      timerMinutes.textContent = getTime.minutes;
+      timerSeconds.textContent = getTime.seconds;
+
+      if (getTime.timeRemaining <= 0) {
+        clearInterval(intervalId);
+      }
+    }, 1000);
+  };
+  updateClock();
+};
+
+export default timer;
