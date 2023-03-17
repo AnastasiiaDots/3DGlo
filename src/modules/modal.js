8bbe5ea75a863d512131cd667c911e7e9f6613a5
Anastasiia Dotsenko <anastasiia_dots@yahoo.com>
2023-03-17 18:06:52 +1300

    modal animation n screen size stop animation

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -3,10 +3,23 @@ const modal = () => {
   const buttons = document.querySelectorAll(".popup-btn");
   const closeBtn = modal.querySelector(".popup-close");
 
+  const animateModal = () => {
+    modal.style.display = "block";
+    if (window.innerWidth < 768) {
+      return;
+    }
+    modal.style.opacity = 0;
+
+    let opacity = 0;
+    const fadeIn = setInterval(() => {
+      opacity += 0.05;
+      modal.style.opacity = opacity;
+      if (opacity >= 1) clearInterval(fadeIn);
+    }, 20);
+  };
+
   buttons.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      modal.style.display = "block";
-    });
+    btn.addEventListener("click", animateModal);
   });
 
   closeBtn.addEventListener("click", () => {
