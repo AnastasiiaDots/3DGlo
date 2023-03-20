fbcf7d25275d35c6436ab11c84112002610db0d4
Anastasiia Dotsenko <anastasiia_dots@yahoo.com>
2023-03-21 11:53:00 +1300

    added validatoin for calc

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
+import calc from "./modules/cacl";
 
 timer("18 march 2023");
 menu();
 modal();
+calc();
