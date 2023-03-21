4edecff8bac24da22c428f248f7ae3ec06ca603c
Anastasiia Dotsenko <anastasiia_dots@yahoo.com>
2023-03-22 10:13:47 +1300

    fixed calc

--- src/modules/cacl.js
+++ src/modules/cacl.js
@@ -1,35 +1,18 @@
 const calc = () => {
-  const numberPattern = /^\d+$/;
-
   const squareInput = document.querySelector(".calc-square");
   const countInput = document.querySelector(".calc-count");
   const dayInput = document.querySelector(".calc-day");
 
   squareInput.addEventListener("input", (e) => {
-    const inputValue = e.target.value.trim();
-    const isValid = inputValue !== "" && numberPattern.test(inputValue);
-    const isError = !isValid;
-    if (isError) {
-      alert("Пожалуйста, введите только числа.");
-    }
+    e.target.value = e.target.value.replace(/\D+/, "");
   });
 
   countInput.addEventListener("input", (e) => {
-    const inputValue = e.target.value.trim();
-    const isValid = inputValue !== "" && numberPattern.test(inputValue);
-    const isError = !isValid;
-    if (isError) {
-      alert("Пожалуйста, введите только числа.");
-    }
+    e.target.value = e.target.value.replace(/\D+/, "");
   });
 
   dayInput.addEventListener("input", (e) => {
-    const inputValue = e.target.value.trim();
-    const isValid = inputValue !== "" && numberPattern.test(inputValue);
-    const isError = !isValid;
-    if (isError) {
-      alert("Пожалуйста, введите только числа.");
-    }
+    e.target.value = e.target.value.replace(/\D+/, "");
   });
 };
 
